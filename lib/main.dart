7631c32cb148fc4b95d23ea3d921282b6c92e68e
Jacob <jac.snall@gmail.com>
2021-08-26 21:53:32 +0200

    Added style to textbutton

--- lib/main.dart
+++ lib/main.dart
@@ -12,21 +12,33 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: Colors.blue,
-          primaryColor: Color(0xFF8359FB),
-          scaffoldBackgroundColor: Color(0xFF8359FB),
-          textTheme: const TextTheme(
-            bodyText2: TextStyle(color: Colors.white, fontSize: 36),
-          )),
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        primaryColor: Color(0xFF8359FB),
+        scaffoldBackgroundColor: Color(0xFF8359FB),
+        textTheme: const TextTheme(
+          bodyText2: TextStyle(color: Colors.white, fontSize: 36),
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            backgroundColor: Color(0xFF5C3EB0),
+            primary: Colors.white,
+            textStyle: TextStyle(fontSize: 24),
+            minimumSize: Size(158, 52),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+      ),
       //home: MyHomePage(title: 'Flutter Demo Home Page'),
       home: StartView(),
     );
