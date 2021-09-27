2b880f85d1f6db2476e4120f104ac6310e6fe52d
Jacob <jac.snall@gmail.com>
2021-09-27 21:22:42 +0200

    Added buttons to switch months i calender_view

--- lib/Views/calender_view.dart
+++ lib/Views/calender_view.dart
@@ -1,49 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 
-class CalenderView extends StatelessWidget {
+class CalenderView extends StatefulWidget {
   const CalenderView({Key? key}) : super(key: key);
 
   //Asset images
-  final _imageSun = const AssetImage('assets/images/sun.png');
-  final _imageCloud = const AssetImage('assets/images/cloud.png');
-
   @override
-  Widget build(BuildContext context) {
-    var box = Hive.box('AppData');
-    final currentStreak = box.get('currentStreak', defaultValue: 0);
-    final longestStreak = box.get('longestStreak', defaultValue: 0);
-
-    return Scaffold(
-      appBar: AppBar(
-          //title: const Text('history'),
-          ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 10, right: 10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Flexible(
-                flex: 2,
-                child: PageView.builder(
-                  reverse: true,
-                  itemBuilder: _buildCalenderPage,
-                ),
-              ),
-              Flexible(
-                child: Text(
-                  'Current streak: ${currentStreak.toString()}\n'
-                  'Longest streak: ${longestStreak.toString()}',
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+  State<CalenderView> createState() => _CalenderViewState();
 
   static const _months = [
     'January',
@@ -74,6 +37,52 @@ class CalenderView extends StatelessWidget {
     30,
     31,
   ];
+}
+
+class _CalenderViewState extends State<CalenderView> {
+  final _imageSun = const AssetImage('assets/images/sun.png');
+  final _imageCloud = const AssetImage('assets/images/cloud.png');
+
+  final PageController _pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    var box = Hive.box('AppData');
+    final currentStreak = box.get('currentStreak', defaultValue: 0);
+    final longestStreak = box.get('longestStreak', defaultValue: 0);
+
+    return Scaffold(
+      appBar: AppBar(
+          //title: const Text('history'),
+          ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 10, right: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(
+                flex: 2,
+                child: PageView.builder(
+                  reverse: true,
+                  itemBuilder: _buildCalenderPage,
+                  controller: _pageController,
+                ),
+              ),
+              Flexible(
+                child: Text(
+                  'Current streak: ${currentStreak.toString()}\n'
+                  'Longest streak: ${longestStreak.toString()}',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   int getDaysInMonth(int month, int year) {
     if (month == 2) {
       if (year % 4 == 0) {
@@ -87,7 +96,7 @@ class CalenderView extends StatelessWidget {
       }
       return 28;
     }
-    return _daysInMonth[month - 1];
+    return CalenderView._daysInMonth[month - 1];
   }
 
   Widget _buildCalenderPage(BuildContext context, int index) {
@@ -123,7 +132,30 @@ class CalenderView extends StatelessWidget {
 
     return Column(
       children: [
-        Text(_months[currentMonth - 1] + ' ' + currentYear.toString()),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              onPressed: () {
+                _pageController.jumpToPage(_pageController.page!.toInt() + 1);
+              },
+              icon: const Icon(Icons.arrow_back),
+              color: Colors.white,
+            ),
+            Text(CalenderView._months[currentMonth - 1] +
+                ' ' +
+                currentYear.toString()),
+            IconButton(
+              onPressed: () {
+                if (_pageController.page! > 0) {
+                  _pageController.jumpToPage(_pageController.page!.toInt() - 1);
+                }
+              },
+              icon: const Icon(Icons.arrow_forward),
+              color: Colors.white,
+            )
+          ],
+        ),
         Flexible(
           child: GridView.count(
             crossAxisCount: 7,
